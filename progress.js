a48f8d330cfd40c39ad63cad0027e0b6054b8900
mrozicki-hub <mrozicki@gmail.com>
2026-07-25 20:48:26 +0200

    Add files via upload

---
+++ progress.js
@@ -0,0 +1,6 @@
+// Ten plik przechowywany jest w Twoim repozytorium na GitHubie.
+// Zawiera całą historię Twoich powtórek i stanu pamięci.
+
+window.myProgressData = {
+  // Tutaj zapisują się interwały powtórek dla każdego słówka
+};
