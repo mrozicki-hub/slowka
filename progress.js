99069e91eeb966f2e03edb213f1af9326fd93568
mrozicki-hub <mrozicki@gmail.com>
2026-07-26 07:20:05 +0200

    Auto-update progress.js: 26.07.2026, 07:20:05

--- progress.js
+++ progress.js
@@ -1,6 +1,87 @@
-// Ten plik przechowywany jest w Twoim repozytorium na GitHubie.
-// Zawiera całą historię Twoich powtórek i stanu pamięci.
-
 window.myProgressData = {
-  // Tutaj zapisują się interwały powtórek dla każdego słówka
+  "w_Q3plxZvEh19IZWxsbw==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596636999
+  },
+  "w_RHppxJlrdWrEmV9UaGFuayB5b3U=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596632897
+  },
+  "w_S3NpxIXFvGthX0Jvb2s=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596633114
+  },
+  "w_UGllc19Eb2c=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596633331
+  },
+  "w_S290X0NhdA==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596633531
+  },
+  "w_SmFixYJrb19BcHBsZQ==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596633747
+  },
+  "w_S29tcHV0ZXJfQ29tcHV0ZXI=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596633964
+  },
+  "w_U2Ftb2Now7NkX0Nhcg==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596634182
+  },
+  "w_U3prb8WCYV9TY2hvb2w=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596634414
+  },
+  "w_V29kYV9XYXRlcg==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596634631
+  },
+  "w_RHppZcWEX0RheQ==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596634864
+  },
+  "w_Tm9jY19OaWdodA==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596635097
+  },
+  "w_U2tydXB1bGF0bnkgLyBEcm9iaWF6Z293eV9NZXRpY3Vsb3Vz": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596635315
+  },
+  "w_TmllamFzbnkgLyBEd3V6bmFjem55X0FtYmlndW91cw==": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596635514
+  },
+  "w_V3N6ZWNob2JlY255X1ViaXF1aXRvdXM=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596635848
+  },
+  "w_UHJ6ZW1pamFqxIVjeSAvIFVsb3RueV9UcmFuc2llbnQ=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596636098
+  },
+  "w_WmFyYWRueSAvIFBvbXlzxYJvd3lfUmVzb3VyY2VmdWw=": {
+    "status": "30d",
+    "interval": 30,
+    "nextReviewDate": 1787596636581
+  }
 };
